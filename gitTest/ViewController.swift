c8d3f28c4a096a67c96da9a0f7f51536a1e0275c
Romaine De La Cruz <r2dcruz@gmail.com>
2018-07-31 17:21:47 -0700

    Initial Commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Romaine De La Cruz on 7/31/18.
+//  Copyright © 2018 r2dcruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
